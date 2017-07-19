b326e437d4d9aab90abfb64df954c1eb9245d007
嵇明新 <jimingxin@liangsichengdeMac-mini.local>
2017-07-19 16:16:13 +0800

    First Version

---
+++ ServerScoketDemo/ServerScoketDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ServerScoketDemo
+//
+//  Created by 嵇明新 on 2017/7/19.
+//  Copyright © 2017年 嵇明新. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
